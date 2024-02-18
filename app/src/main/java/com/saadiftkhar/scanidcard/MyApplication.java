1b285937720917b2d70eec0e5aa86ccf0279bd27
ENT22060216\ENT22060216 <Saad>
2024-02-19 02:22:52 +0500

    update camera activity layout, convert into kotlin and implement view binding

--- app/src/main/java/com/saadiftkhar/scanidcard/MyApplication.java
+++ app/src/main/java/com/saadiftkhar/scanidcard/MyApplication.java
@@ -3,12 +3,6 @@ package com.saadiftkhar.scanidcard;
 import android.app.Application;
 
 
-/**
- * Author       wildma
- * Github       https://github.com/wildma
- * Date         2019/04/28
- * Desc	        ${MyApplication}
- */
 public class MyApplication extends Application {
 
     @Override
